7c47d846a06d61d6015c7e8f60a6b7feea4d278d
Shraddha Ravishankar <shraddha.ravi18@gmail.com>
2017-02-22 18:20:25 -0800

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,32 @@
+import edu.uci.ics.crawler4j.crawler.CrawlConfig;
+import edu.uci.ics.crawler4j.crawler.CrawlController;
+import edu.uci.ics.crawler4j.fetcher.PageFetcher;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
+import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
+
+public class Controller {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		String crawlStorageFolder="/Users/shraddharavishankar/Documents/USC Notes/IR/data/crawl";
+		int numberOfCrawlers=10;
+	
+
+	CrawlConfig config = new CrawlConfig();
+	config.setIncludeBinaryContentInCrawling(true);
+	config.setCrawlStorageFolder(crawlStorageFolder);
+	config.setMaxPagesToFetch(20000);
+	config.setMaxDepthOfCrawling(16);
+    config.setPolitenessDelay(300);
+    config.setUserAgentString("Hello from USC");
+	
+	
+	 PageFetcher pageFetcher = new PageFetcher(config);
+     RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+     RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+     CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+
+     controller.addSeed("http://www.latimes.com/");
+     controller.start(MyCrawler.class, numberOfCrawlers);
+	}
+}
